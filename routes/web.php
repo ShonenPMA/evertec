0efc36ca01fe0de43cbce138700517f2236a6ec5
Jhon Ayrton Saavedra Castañeda <vagrant@homestead>
2021-05-03 20:30:45 +0000

    cargando las rutas en web, solo login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('web.welcome');
 })->name('welcome');
+
+
+Auth::routes([
+    'login' => true,
+    'register' => false,
+    'reset' =>false,
+    'verifiy' => false,
+    'confirm' => false
+    ]);
