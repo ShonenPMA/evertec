b64a0c0bdd36d3ea9be24eb88751ccb75c65eb82
Jhon Ayrton Saavedra Castañeda <vagrant@homestead>
2021-05-04 15:31:36 +0000

    migracion,seeder y factory de los  productos

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $faker = \Faker\Factory::create();
+        \Bezhanov\Faker\ProviderCollectionHelper::addAllProvidersTo($faker);
+        $name = $faker->unique()->productName;
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $faker->text(),
+            'abstract' => $faker->text(50),
+            'price' => $faker->numberBetween(300,1000),
+            'discount' => $faker->numberBetween(0,80) / 100
+        ];
+    }
+}
---
+++ database/migrations/2021_05_04_144250_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+
+            $table->text('description');
+            $table->text('abstract');
+
+            $table->string('image_url')->default('https://source.unsplash.com/1600x900/?jewelry');
+            $table->integer('total_views')->default(0);
+            $table->integer('total_orders')->default(0);
+
+            $table->float('price');
+            $table->float('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
